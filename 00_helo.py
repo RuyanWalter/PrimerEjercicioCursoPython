b50d348529d48375e1be635d806ac02c4ec4e7c8
RuyanWalter <sirin23ruyan@gmail.com>
2024-07-12 02:17:21 -0600

    update

---
+++ 00_helo.py
@@ -0,0 +1,27 @@
+# Esto es un comentario ´Hola Mundo´
+# Nuestro hola Mundo en python
+
+#Esta es una cadena de texto se puede poner dentro 
+#Comilla doble y simple
+print("Hola Python")
+print('Hola Mundo')
+
+#El type es para consultar el tipo de dato 
+print(type("Soy un dato str")) #Tipo de dato 'str'
+print(type(5)) #Tipo de dato 'int
+print(type(1.5)) #Tipo de dato 'float'
+print(type(True)) #Tipo de dato 'bool
+print(type(1 + 2j)) #Tipo de dato 'complejo'
+
+"""
+Este es un 
+comentario 
+en varias líneas
+"""
+'''
+Este tambien es un 
+comentario 
+en varias líneas
+'''
+
+
---
+++ 01_Variables.py
@@ -0,0 +1,17 @@
+#Vamos aprender las variables
+
+#Esta es una variable string
+my_string_variable = "Esta es mi variable"
+print(my_string_variable) 
+
+#Esta es una variable INT
+my_int_variable = 5
+print(my_int_variable) 
+
+#Esta es una variable BOOL
+my_bool_variable = False
+print(my_bool_variable)
+
+#Se realiza el llamado a disferentes variables 
+print(my_string_variable,my_int_variable)
+
